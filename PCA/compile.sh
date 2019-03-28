b14e5ab383fa786c759a5b04fa9370bf6b381d87
jngaravitoc <jngaravitoc@email.arizona.edu>
2019-03-28 09:15:23 -0700

    code

--- PCA/compile.sh
+++ PCA/compile.sh
@@ -1,3 +1,3 @@
-gcc -Wall -I/usr/local/include -c covariance_matrix.c -lm -fopenmp
-gcc -Wall -L/usr/local/lib covariance_matrix.o -lgsl -lgslcblas -lm -fopenmp
+gcc -Wall -I/usr/local/include -c covariance_matrix.c -lm -fopenmp -w
+gcc -Wall -L/usr/local/lib covariance_matrix.o -lgsl -lgslcblas -lm -fopenmp -w
 #./a.out > cov_mat_sph_1x1.txt
