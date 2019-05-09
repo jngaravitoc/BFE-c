7a6234603ba4e5bdf23c7b700637b4411408bf10
jngaravitoc <jngaravitoc@email.arizona.edu>
2019-05-09 16:51:53 -0700

    compile and run

--- PCA/compile.sh
+++ PCA/compile.sh
@@ -1,3 +1,3 @@
-gcc -Wall -I/usr/local/include -c covariance_matrix.c   -lm -fopenmp -O2 -w
-gcc -Wall -L/usr/local/lib covariance_matrix.o  -lgsl -lgslcblas -lm -fopenmp -O2 -w
+gcc -Wall -I/usr/local/include  -c io.c  covariance_matrix.c   -lm -fopenmp -O2 
+gcc -Wall -L/usr/local/lib io.o covariance_matrix.o  -lgsl -lgslcblas -lm -fopenmp -O2 
 #./a.out > cov_mat_sph_1x1.txt
--- PCA/run.sh
+++ PCA/run.sh
@@ -1,1 +1,2 @@
-./a.out 5 5 410833 > cov_mat_prolate_5x5.txt
+OMP_NUM_THREADS=1 ./a.out 20 0 100000000 MW_100M_b1_dm_part_1e8.txt test.txt
+
