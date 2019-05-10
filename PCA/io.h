981be7bcb001cbe62462d8928001f4ff9ecfd0bf
Juan Nicolas Garavito Camargo <jngaravitoc@email.arizona.edu>
2019-05-10 15:52:16 -0700

    Delete io.h

--- PCA/io.h
+++
@@ -1,5 +0,0 @@
-void read_data(char *filename, int n_points, double *r, double *theta, double *phi, double *m, double r_s);
-
-void write_data(char *filename, int n_max, int l_max, double S[n_max][l_max][l_max], double T[n_max][l_max][l_max]);
-
-void rand_sampling(int n_points, double *r_rand, double *theta_rand, double *phi_rand, double *M_rand, double *r, double *theta, double *phi, double *M);
