dbb6f54b3e4e86fb4eee7dd06321be7c86c7cf28
Juan Nicolas Garavito Camargo <jngaravitoc@email.arizona.edu>
2019-05-10 15:49:40 -0700

    Delete compile.sh

--- PCA/compile.sh
+++
@@ -1,3 +0,0 @@
-gcc -Wall -I/usr/local/include  -c io.c  covariance_matrix.c   -lm -fopenmp -O2 
-gcc -Wall -L/usr/local/lib io.o covariance_matrix.o  -lgsl -lgslcblas -lm -fopenmp -O2 
-#./a.out > cov_mat_sph_1x1.txt
