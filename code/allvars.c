7dd491174b58aeb28e18c60bff57403a218553dc
jngaravitoc <jngaravitoc@email.arizona.edu>
2019-06-20 14:31:26 -1000

    allvars files

---
+++ code/allvars.c
@@ -0,0 +1,20 @@
+#include "allvars.h"
+
+int charlength = 16;
+char tmpstr1[nchar];                                                                                                                                                    
+char tmpstr2[nchar];                                                                     
+char InputDir[nchar];                                                                                                                                             
+char InputFile[nchar];                                                                                                                                                
+char OutputDir[nchar];                                                                                                                                                   
+char OutputFile[nchar];                                                                                                                                                  
+
+int *InputFormat;                                                                                                                                                       
+int *OutputFormat;                                                                                                                                                      
+int *Nparticles;                                                                                                                                                        
+int *nmax;                                                                                                                                                              
+int *lmax;                                                                                                                                                              
+int *cov_matrix;                                                                                                                                                        
+int *BootstrapSample;                                                                                                                                                   
+int *Nsampling;                                                                                                                                                         
+
+double r_s;          
---
+++ code/allvars.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+                                                                                                                                                                        
+int nchar=16;
+
+extern char InputDir[nchar];       
+extern char InputFile[nchar];                                                                                                                                 
+extern char OutputDir[nchar];
+extern char OutputFile[nchar];
+
+extern int *InputFormat;
+extern int *OutputFormat;
+extern int *Nparticles;    
+extern int *nmax;
+extern int *lmax;   
+extern int *cov_matrix;                                                                                                                                             
+extern int *BootstrapSample;
+extern int *Nsampling;
+extern double r_s;
